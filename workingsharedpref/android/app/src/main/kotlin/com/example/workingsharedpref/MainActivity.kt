040135577650c1a5f820837556312e8efc9bd203
Benjamin <ashishbenjamin2002@gmail.com>
2024-09-16 13:43:09 +0530

    second commit

---
+++ workingsharedpref/android/app/src/main/kotlin/com/example/workingsharedpref/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.workingsharedpref
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ workingwithjson/android/app/src/main/kotlin/com/example/workingwithjson/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.workingwithjson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
